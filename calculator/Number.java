7ec9244a5e07cda260558fd781ef4ca855870aaa
Julian97s <124013438+Julian97s@users.noreply.github.com>
2024-10-08 14:53:24 -0400

    No advance, trying to find structure

---
+++ calculator/Number.java
@@ -0,0 +1,9 @@
+package calculator;
+
+public class Number extends Calculator{
+    private double number;
+
+    public Number(double g_number){
+        this.number = number;
+    }
+}
---
+++ calculator/Operators.java
@@ -0,0 +1,8 @@
+package calculator;
+
+public abstract class Operators extends Calculator{
+    double left_side;
+    double right_side;
+    
+    public abstract double eval();
+}
