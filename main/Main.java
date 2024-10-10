b0d7880974e1b183605b56b4ce6bfe0a6695c422
Julian97s <124013438+Julian97s@users.noreply.github.com>
2024-10-10 19:08:17 -0400

    line 28 is not evaluating true when using str_operation.contains

--- main/Main.java
+++ main/Main.java
@@ -1,4 +1,23 @@
 package main;
+
+import calculator.Token;
+import java.util.*;
 public class Main {
+    public static void main(String[] args) {
     
+        System.out.println("\nPlease give an operation to be evaluated:\n");
+        Scanner s = new Scanner(System.in);
+        String result = "Invalid input";
+        if (s.hasNext() ){
+            result = s.nextLine();
+            Token calculation = Token.parseToken(result);            
+            System.out.println(calculation.eval());
+        } else {
+            System.out.println(result);
+        }
+    }
+    
+
+    
+     
 }
